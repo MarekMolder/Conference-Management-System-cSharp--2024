b9105382ef14038165a269841b7061e4d650b2db
marek <marekmolder@gmail.com>
2025-01-01 19:52:46 +0200

    Exam Algus tehtud

---
+++ Exam/DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class AppDbContext : DbContext
+{
+    // public DbSet<Booking> Bookings { get; set; }
+    // public DbSet<Client> Clients { get; set; }
+    // public DbSet<Table> Tables { get; set; }
+    // public DbSet<Restaurant> Restaurants { get; set; }
+      
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ Exam/DAL/FileHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace DAL;
+
+public static class FileHelper
+{
+    public static string BasePath = Environment
+                                        .GetFolderPath(Environment.SpecialFolder.UserProfile)
+                                    + Path.DirectorySeparatorChar + "Restaurant" + Path.DirectorySeparatorChar;
+}
